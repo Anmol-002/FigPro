2afaf02ca8c024f1575fb10b2bf23f1c10fe4598
Anmol <109303519+Anmol-002@users.noreply.github.com>
2024-03-10 15:02:22 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,6 @@ const workSans = Work_Sans({
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
   <html lang='en'>
     <head>
-      <link rel="icon" href="/favicon.ico" />
       <Temp />
     </head>
     <body className={`${workSans.className} bg-primary-grey-200`}>
